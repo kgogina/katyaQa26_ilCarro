cf0315172eeff51a6ffecd815c855bb873a81d99
kgogina <40139831+kgogina@users.noreply.github.com>
2021-01-13 12:22:36 +0200

    Created Registration test, in Progress

---
+++ src/test/java/com/example/ilcarro/qa/RegistrationTest.java
@@ -0,0 +1,30 @@
+package com.example.ilcarro.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationTest {
+    WebDriver wd;
+
+
+    @BeforeMethod
+    public void setUp (){
+        wd = new ChromeDriver();
+    wd.get("https://google.com");
+    }
+
+    @Test
+    public void testRegistration (){
+
+    }
+
+    @AfterMethod
+    public void tearDown (){
+
+    }
+
+}
