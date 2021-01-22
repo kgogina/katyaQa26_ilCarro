00ac0392268f0fc58b507493856749cdda08ab4e
kgogina <40139831+kgogina@users.noreply.github.com>
2021-01-22 14:49:40 +0200

    testCarCreation

--- src/test/java/com/example/ilcarro/qa/application/CarHelper.java
+++ src/test/java/com/example/ilcarro/qa/application/CarHelper.java
@@ -17,7 +17,28 @@ public class CarHelper extends HelperBase{
         return  isElementPresent(By.xpath("//h3[contains(., 'Let the car work')]"));
     }
 
-    public void fillCarForm(String location){
-        type(By.cssSelector(""), location);
+    public void fillCarForm(String country,String address, String distance, String serial_number, String brand, String model, String year, String engine, String fuel_consumption, String fuel, String transmition, String WD, String horsepower, String torque, String doors, String seats, String clasS, String about, String feature, String price){
+        type(By.cssSelector(".country"), country);
+        type(By.cssSelector(".address"), address);
+        type(By.cssSelector(".distance_included"), distance);
+        type(By.cssSelector(".serial_number"), serial_number);
+        type(By.cssSelector(".brand"), brand);
+        type(By.cssSelector(".model"), model);
+        type(By.cssSelector(".year"), year);
+        type(By.cssSelector(".engine"), engine);
+        type(By.cssSelector(".fuel_consumption"), fuel_consumption);
+        type(By.cssSelector(".fuel"), fuel);
+        type(By.cssSelector(".transmition"), transmition);
+        type(By.cssSelector(".wd"), WD);
+        type(By.cssSelector(".horsepower"), horsepower);
+        type(By.cssSelector(".torque"), torque);
+        type(By.cssSelector(".doors"), doors);
+        type(By.cssSelector(".seats"), seats);
+        type(By.cssSelector(".class"), clasS);
+        type(By.cssSelector("[name='about']"), about);
+        type(By.cssSelector(".type_feature"), feature);
+        type(By.cssSelector(".price"), price);
     }
-}
+
+    }
+
--- src/test/java/com/example/ilcarro/qa/tests/CarCreationTests.java
+++ src/test/java/com/example/ilcarro/qa/tests/CarCreationTests.java
@@ -3,13 +3,15 @@ package com.example.ilcarro.qa.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CarCreationTests  extends TestBase{
+public class CarCreationTests extends TestBase {
     @Test
-    public void testCarCreation (){
+    public void testCarCreation() throws InterruptedException {
         app.getCarHelper().initAddingNewCar();
-        Assert.assertTrue(app.getCarHelper().isCarCreationFormPresent());
-
-        app.getCarHelper().fillCarForm("TelAviv");
+        //Assert.assertTrue(app.getCarHelper().isCarCreationFormPresent());
+        app.getCarHelper().fillCarForm("Israel", "Herzel1", "500", "233-67-22", "Honda", "Civic", "2015", "HC325679YH",
+                "6.5", "petrol", "full Drive", "4WD", "149", "120", "5", "5", "C", "Very good car",
+                "no smoking", "55");
+        app.getCarHelper().pause(3000);
         app.getCarHelper().clickYallaButtom();
     }
 
