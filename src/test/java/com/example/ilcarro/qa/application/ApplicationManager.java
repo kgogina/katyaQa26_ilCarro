4918fc4f86635f92243bb115b0cf781f793d22f9
kgogina <40139831+kgogina@users.noreply.github.com>
2021-01-28 14:13:16 +0200

    added eventFiringWebDriver - for logging throwable

--- src/test/java/com/example/ilcarro/qa/application/ApplicationManager.java
+++ src/test/java/com/example/ilcarro/qa/application/ApplicationManager.java
@@ -1,31 +1,68 @@
 package com.example.ilcarro.qa.application;
 
+import com.google.common.io.Files;
 import com.sun.javafx.geom.Edge;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    WebDriver wd;
+    static EventFiringWebDriver wd;
     UserHelper userHelper;
     CarHelper carHelper;
     private String browser;
     Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+
+        HelperBase helperBase = new HelperBase(wd);
+        public MyListener() {
+            super ();
+        }
+        Logger logger = LoggerFactory.getLogger(MyListener.class);
+
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println("Start search" + by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by + "found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver wd) {
+            logger.error(throwable.toString());
+
+            String pathToScreenshot = "src/test/screenshorts/screen-" + System.currentTimeMillis()+ ".png";
+            helperBase.takeScreenShort(pathToScreenshot);
+            logger.error(pathToScreenshot);
+            }
+        }
+
+
 
     public ApplicationManager(String browser) {
         this.browser = browser;
     }
 
 
+
     public void start() {
         if(browser.equals(BrowserType.CHROME)){
-            wd = new ChromeDriver();
+            wd = new EventFiringWebDriver ( new ChromeDriver());
+            wd.register(new MyListener());
         }
 
         wd.navigate().to("https://ilcarro-dev-v1.firebaseapp.com/");
